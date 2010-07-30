03c84582c1feb9641630aeafdccf99daf0221ba9
Nathan Hamblen <nathanh@meetup.com>
2010-07-30 19:30:29 -0400

    with jquery

--- local.js
+++ local.js
@@ -1,3 +1,7 @@
+var wjq$ = jQuery.noConflict();
+function with_jquery(block) {
+  block(jQuery);
+}
 function api_call(path, params) {
   return "http://api.meetup.com" + path + "?callback=?&" + wjq$.param(wjq$.extend({ key: $api_key }, params));
 }
