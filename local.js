db6fdff75412654660e834521c002e764c22bdc1
Steve Aquillano <steveaquillano@gmail.com>
2010-08-05 15:29:54 -0400

    optimize

--- local.js
+++ local.js
@@ -1,6 +1,6 @@
 jQuery.noConflict();
 function with_jquery(block) {
-  block(jQuery);
+  block(jQuery, document.body);
 }
 function api_call(path, params) {
   return "http://api.meetup.com" + path + "?callback=?&" + jQuery.param(jQuery.extend({ key: $api_key }, params));
