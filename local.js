9621b55c48ae5a43bd002cfb0b078141b5bff562
Nathan Hamblen <nathanh@meetup.com>
2010-08-06 13:22:01 -0400

    object declaration

--- local.js
+++ local.js
@@ -1,8 +1,9 @@
 jQuery.noConflict();
-var	mup_widget = {};
-mup_widget.with_jquery = function(block) {
-	block(jQuery, document.body);
-};
-mup_widget.api_call = function(path, params) {
-	return "http://api.meetup.com" + path + "?callback=?&" + jQuery.param(jQuery.extend({ key: $api_key }, params));
+var mup_widget = {
+    with_jquery: function(block) {
+        block(jQuery, document.body);
+    },
+    api_call: function(path, params) {
+        return "http://api.meetup.com" + path + "?callback=?&" + jQuery.param(jQuery.extend({ key: $api_key }, params));
+    }
 };
